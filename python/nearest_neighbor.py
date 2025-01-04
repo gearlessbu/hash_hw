3d8e745d739bd8e1258b8ccd6bb5d7338fe5f57d
Jiamu Bu <bjm21@mails.tsinghua.edu.cn>
2025-01-04 20:22:50 +0800

    mean approx rate nn

--- python/nearest_neighbor.py
+++ python/nearest_neighbor.py
@@ -18,11 +18,12 @@ def compare_KDTree_and_LSH(sample_num=1):
     probe = np.random.rand(dim,) * (np.linalg.norm(vectors[0]) + np.linalg.norm(vectors[10] + np.linalg.norm(vectors[20])))\
         / 3 / np.sqrt(dim) * 2.4
     
-    LSH_time, KDTree_time = 0., 0.
+    LSH_time, KDTree_time, c = 0., 0., 0.
 
     for _ in range(sample_num):
         probe = np.random.rand(dim,) * (np.linalg.norm(vectors[0]) + np.linalg.norm(vectors[10] + np.linalg.norm(vectors[20])))\
-            / 3 / np.sqrt(dim) * 2.4
+            / 3 / np.sqrt(dim) * 0.4
+        # probe = vectors[np.random.randint(0, vectors.shape[0])] * (1 + 0.3 * np.random.rand(dim,))
 
         # LSH
         start_time = time.time()
@@ -38,7 +39,11 @@ def compare_KDTree_and_LSH(sample_num=1):
         KDTree_time += time.time() - start_time
         # print("KDTree time: {} seconds".format(time.time() - start_time))
 
+        # print("c=", distance_LSH / distance)
+        c += distance_LSH / distance
+
     print("LSH mean time: {} seconds".format(LSH_time / sample_num))
     print("KDTree mean time: {} seconds".format(KDTree_time / sample_num))
+    print("LSH mean approx rate time: {} seconds".format(KDTree_time / sample_num))
 
 compare_KDTree_and_LSH(20)
