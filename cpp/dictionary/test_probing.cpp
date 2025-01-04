35842462514b27fda36b219e69b1d660d9c9dbba
Jiamu Bu <bjm21@mails.tsinghua.edu.cn>
2025-01-04 17:03:35 +0800

    tabulation exp finish

--- cpp/dictionary/test_probing.cpp
+++ cpp/dictionary/test_probing.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <numeric>
 
@@ -19,113 +20,45 @@ float mean_probing_cnt(HashTable& table, std::shared_ptr<std::vector<uint32_t>> 
     return probing_numsum / keys_num;
 }
 
-std::vector<size_t> probing_hist(HashTable& table,
-                                 std::shared_ptr<std::vector<uint32_t>> structured_keys)
+float mean_hash_time(const std::string& hash_type,
+                     std::shared_ptr<std::vector<uint32_t>> structured_keys)
 {
     size_t keys_num = structured_keys->size();
-    std::vector<size_t> key_probing_nums;
-    key_probing_nums.resize(3000, 0);
+    std::vector<float> mean_probing_cnts;
+    std::vector<float> mean_insert_nanosec;
+    std::shared_ptr<Hash> hashfunc_ptr;
+    if (hash_type == "UnivMultShift")
+    {
+        hashfunc_ptr = std::make_shared<UnivMultShift>(32, 21);
+    }
+    else if (hash_type == "Indpd2MultShift")
+    {
+        hashfunc_ptr = std::make_shared<Indpd2MultShift>(32, 21);
+    }
+    else if (hash_type == "SimpleTabulation")
+    {
+        hashfunc_ptr = std::make_shared<SimpleTabulation>();
+    }
+    else if (hash_type == "Indpd5TZTable")
+    {
+        hashfunc_ptr = std::make_shared<Indpd5TZTable>();
+    }
+    else if (hash_type == "Indpd5MersennePrime")
+    {
+        hashfunc_ptr = std::make_shared<PolynomialMersenneHash>(5, 61, 32);
+    }
+    else
+    {
+        std::cerr << "Unknown hash type.\n";
+    }
+    auto start = std::chrono::high_resolution_clock::now();
     for (size_t i = 0; i < keys_num; ++i)
     {
-        size_t probing_num = table.insert(structured_keys->at(i));
-        if (probing_num < 3000) key_probing_nums[probing_num] += 1;
+        auto index = hashfunc_ptr->hash(structured_keys->at(i));
     }
-    return key_probing_nums;
-}
-
-// void test_probing(std::shared_ptr<std::vector<uint32_t>> structured_keys)
-// {
-//     std::shared_ptr<Hash> SimpleTabulation_ptr = std::make_shared<SimpleTabulation>();
-//     HashTable SimpleTabulation_table(1 << 21, SimpleTabulation_ptr);
-//     std::vector<size_t> SimpleTabulation_probing_cnt =
-//         probing_cnt(SimpleTabulation_table, structured_keys);
-//     SaveVectortxt(SimpleTabulation_probing_cnt, "tmp/SimpleTabulation_probing_cnt.txt");
-//     // std::shared_ptr<Hash> UnivMultShift_ptr = std::make_shared<UnivMultShift>(32, 21);
-//     // HashTable UnivMultShift_table(1 << 21, UnivMultShift_ptr);
-//     // std::vector<size_t> UnivMultShift_probing_cnt =
-//     //     probing_cnt(UnivMultShift_table, structured_keys);
-//     // SaveVectortxt(UnivMultShift_probing_cnt, "tmp/UnivMultShift_probing_cnt.txt");
-// }
-
-void test_SimpleTabulation_probing_100(std::shared_ptr<std::vector<uint32_t>> structured_keys)
-{
-    size_t keys_num = structured_keys->size();
-    std::vector<float> mean_probing_cnts;
-    for (size_t i = 0; i < 100; ++i)
-    {
-        std::shared_ptr<Hash> SimpleTabulation_ptr = std::make_shared<SimpleTabulation>();
-        HashTable SimpleTabulation_table(1 << 21, SimpleTabulation_ptr);
-        float SimpleTabulation_probing_cnt_mean =
-            mean_probing_cnt(SimpleTabulation_table, structured_keys);
-        mean_probing_cnts.push_back(SimpleTabulation_probing_cnt_mean);
-    }
-    SaveVectortxt(mean_probing_cnts, "tmp/SimpleTabulation_mean_probing_cnts.txt");
-}
-
-void test_Indpd5TZTable_probing_100(std::shared_ptr<std::vector<uint32_t>> structured_keys)
-{
-    size_t keys_num = structured_keys->size();
-    std::vector<float> mean_probing_cnts;
-    for (size_t i = 0; i < 100; ++i)
-    {
-        std::shared_ptr<Hash> hashfunc_ptr = std::make_shared<Indpd5TZTable>();
-        HashTable hash_table(1 << 21, hashfunc_ptr);
-        float hash_probing_cnt_mean = mean_probing_cnt(hash_table, structured_keys);
-        mean_probing_cnts.push_back(hash_probing_cnt_mean);
-    }
-    SaveVectortxt(mean_probing_cnts, "tmp/Indpd5TZTable_mean_probing_cnts.txt");
-}
-
-void test_UnivMultShift_probing_100(std::shared_ptr<std::vector<uint32_t>> structured_keys)
-{
-    size_t keys_num = structured_keys->size();
-    std::vector<float> mean_probing_cnts;
-    for (size_t i = 0; i < 100; ++i)
-    {
-        std::shared_ptr<Hash> UnivMultShift_ptr = std::make_shared<UnivMultShift>(32, 21);
-        HashTable UnivMultShift_table(1 << 21, UnivMultShift_ptr);
-        float UnivMultShift_probing_cnt_mean =
-            mean_probing_cnt(UnivMultShift_table, structured_keys);
-        // std::cout << "UnivMultShift_probing_cnt_mean=" << UnivMultShift_probing_cnt_mean
-        //           << std::endl;
-        mean_probing_cnts.push_back(UnivMultShift_probing_cnt_mean);
-    }
-    SaveVectortxt(mean_probing_cnts, "tmp/UnivMultShift_mean_probing_cnts.txt");
-}
-
-// Indpd2MultShift
-
-void test_Indpd2MultShift_probing_100(std::shared_ptr<std::vector<uint32_t>> structured_keys)
-{
-    size_t keys_num = structured_keys->size();
-    std::vector<float> mean_probing_cnts;
-    for (size_t i = 0; i < 100; ++i)
-    {
-        std::shared_ptr<Hash> hashfunc_ptr = std::make_shared<Indpd2MultShift>(32, 21);
-        HashTable hash_table(1 << 21, hashfunc_ptr);
-        float hash_probing_cnt_mean = mean_probing_cnt(hash_table, structured_keys);
-        mean_probing_cnts.push_back(hash_probing_cnt_mean);
-    }
-    SaveVectortxt(mean_probing_cnts, "tmp/Indpd2MultShift_mean_probing_cnts.txt");
-}
-
-void test_Indpd5MersennePrime_probing_100(std::shared_ptr<std::vector<uint32_t>> structured_keys)
-{
-    size_t keys_num = structured_keys->size();
-    std::vector<float> mean_probing_cnts;
-    for (size_t i = 0; i < 100; ++i)
-    {
-        std::shared_ptr<Hash> Indpd5MersennePrime_ptr =
-            std::make_shared<PolynomialMersenneHash>(5, 61, 32);
-        HashTable Indpd5MersennePrime_table(1 << 21, Indpd5MersennePrime_ptr);
-        float Indpd5MersennePrime_probing_cnt_mean =
-            mean_probing_cnt(Indpd5MersennePrime_table, structured_keys);
-        // std::cout << "Indpd5MersennePrime_probing_cnt_mean=" <<
-        // Indpd5MersennePrime_probing_cnt_mean
-        //           << std::endl;
-        mean_probing_cnts.push_back(Indpd5MersennePrime_probing_cnt_mean);
-    }
-    SaveVectortxt(mean_probing_cnts, "tmp/Indpd5MersennePrime_mean_probing_cnts.txt");
+    auto end = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() /
+           float(keys_num);
 }
 
 void mean_probing_100(const std::string& hash_type,
@@ -134,6 +67,7 @@ void mean_probing_100(const std::string& hash_type,
 {
     size_t keys_num = structured_keys->size();
     std::vector<float> mean_probing_cnts;
+    std::vector<float> mean_insert_nanosec;
     for (size_t i = 0; i < 100; ++i)
     {
         std::shared_ptr<Hash> hashfunc_ptr;
@@ -162,25 +96,43 @@ void mean_probing_100(const std::string& hash_type,
             std::cerr << "Unknown hash type.\n";
         }
         HashTable hash_table(1 << 21, hashfunc_ptr);
+        auto start = std::chrono::high_resolution_clock::now();
         float hash_probing_cnt_mean = mean_probing_cnt(hash_table, structured_keys);
+        auto end = std::chrono::high_resolution_clock::now();
+        mean_insert_nanosec.push_back(
+            std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() /
+            float(keys_num));
         mean_probing_cnts.push_back(hash_probing_cnt_mean);
     }
     std::string mean_prob_file;
+    std::string mean_insert_nanosec_file;
     if (dataname.size() == 0)
+    {
+        mean_insert_nanosec_file = "tmp/" + hash_type + "_mean_insert_nanosec.txt";
         mean_prob_file = "tmp/" + hash_type + "_mean_probing_cnts.txt";
+    }
     else
+    {
+        mean_insert_nanosec_file =
+            "tmp/" + dataname + "_" + hash_type + "__mean_insert_nanosec.txt";
         mean_prob_file = "tmp/" + dataname + "_" + hash_type + "_mean_probing_cnts.txt";
+    }
     SaveVectortxt(mean_probing_cnts, mean_prob_file);
+    SaveVectortxt(mean_insert_nanosec, mean_insert_nanosec_file);
 }
 
 int main()
 {
     auto structured_keys = hypercube_data(20);
-    // test_SimpleTabulation_probing_100(structured_keys);
-    // test_UnivMultShift_probing_100(structured_keys);
-    // test_Indpd5MersennePrime_probing_100(structured_keys);
-    // test_Indpd2MultShift_probing_100(structured_keys);
-    // test_Indpd5TZTable_probing_100(structured_keys);
+
+    std::vector<float> mean_hash_times;
+    mean_hash_times.push_back(mean_hash_time("UnivMultShift", structured_keys));
+    mean_hash_times.push_back(mean_hash_time("Indpd2MultShift", structured_keys));
+    mean_hash_times.push_back(mean_hash_time("Indpd5MersennePrime", structured_keys));
+    mean_hash_times.push_back(mean_hash_time("SimpleTabulation", structured_keys));
+    mean_hash_times.push_back(mean_hash_time("Indpd5TZTable", structured_keys));
+    SaveVectortxt(mean_hash_times, "tmp/mean_hash_time.txt");
+
     mean_probing_100("UnivMultShift", structured_keys);
     mean_probing_100("Indpd2MultShift", structured_keys);
     mean_probing_100("Indpd5MersennePrime", structured_keys);
--- python/cum_frac.py
+++ python/cum_frac.py
@@ -29,5 +29,54 @@ plt.xlabel("average probes per insert/delete")
 plt.ylabel("cumulative fraction")
 plt.title("Keys from hyper cube")
 plt.legend()
-plt.savefig("./tmp/hypercube_cumfrac.png", dpi=300, bbox_inches="tight")
+plt.savefig("./tmp/hypercube_cumfrac_probes.png", dpi=300, bbox_inches="tight")
 # plt.show()
+
+
+UnivMultShift_mean_insert_nanosec = np.loadtxt("./tmp/UnivMultShift_mean_insert_nanosec.txt")
+Indpd2MultShift_mean_insert_nanosec = np.loadtxt("./tmp/Indpd2MultShift_mean_insert_nanosec.txt")
+Indpd5MersennePrime_mean_insert_nanosec = np.loadtxt("./tmp/Indpd5MersennePrime_mean_insert_nanosec.txt")
+SimpleTabulation_mean_insert_nanosec = np.loadtxt("./tmp/SimpleTabulation_mean_insert_nanosec.txt")
+Indpd5TZTable_mean_insert_nanosec = np.loadtxt("./tmp/Indpd5TZTable_mean_insert_nanosec.txt")
+
+sorted_nano_UnivMultShift = np.sort(UnivMultShift_mean_insert_nanosec)
+sorted_nano_Indpd2MultShift = np.sort(Indpd2MultShift_mean_insert_nanosec)
+sorted_nano_SimpleTabulation = np.sort(SimpleTabulation_mean_insert_nanosec)
+sorted_nano_Indpd5TZTable = np.sort(Indpd5TZTable_mean_insert_nanosec)
+sorted_nano_Indpd5MersennePrime = np.sort(Indpd5MersennePrime_mean_insert_nanosec)
+
+# Calculate the cumulative fraction
+cumulative_fraction = np.arange(1, len(sorted_nano_UnivMultShift) + 1) / len(sorted_nano_UnivMultShift)
+
+fig, ax = plt.subplots()
+# Plot the cumulative fraction
+plt.plot(sorted_nano_UnivMultShift, cumulative_fraction, linestyle='-', color='b', label="univ-mult-shift")
+plt.plot(sorted_nano_Indpd2MultShift, cumulative_fraction, linestyle='-', color='purple', label="2-indep-mult-shift")
+plt.plot(sorted_nano_SimpleTabulation, cumulative_fraction, linestyle='-', color='r', label="simple-table")
+plt.plot(sorted_nano_Indpd5TZTable, cumulative_fraction, linestyle='-', color='orangered', label="5-indep-TZ-table")
+plt.plot(sorted_nano_Indpd5MersennePrime, cumulative_fraction, linestyle='-', color='g', label="5-indep-Mersenne-prime")
+ax.set_xlim(0, 150)
+ax.set_ylim(0, 1)
+plt.xlabel("average time per insert+delete cycle (nanoseconds)")
+plt.ylabel("cumulative fraction")
+plt.title("Keys from hyper cube")
+plt.legend()
+plt.savefig("./tmp/hypercube_cumfrac_nano.png", dpi=300, bbox_inches="tight")
+# plt.show()
+
+
+data = [
+    ['UnivMultShift'],
+    ['Indpd2MultShift'],
+    ['Indpd5MersennePrime'],
+    ['SimpleTabulation'],
+    ['Indpd5TZTable'],
+]
+meantimes = np.loadtxt("./tmp/mean_hash_time.txt")
+for i in range(5):
+    data[i].append(meantimes[i])
+columns = ['hashing scheme', 'update time (nanoseconds)']
+fig, ax = plt.subplots()
+ax.axis('off')
+table = ax.table(cellText=data, colLabels=columns, loc='center')
+plt.savefig("./tmp/mean_hash_time.png", dpi=300, bbox_inches="tight")
