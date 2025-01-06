ad70dd3bfc9400c6b0661a5543237ae295679240
Jiamu Bu <bjm21@mails.tsinghua.edu.cn>
2025-01-06 17:14:09 +0800

    compare DSH with K-D tree

--- python/nearest_neighbor.py
+++ python/nearest_neighbor.py
@@ -3,6 +3,7 @@ import time
 
 from KDTree import KDTree
 from LSH import LSH
+from DSH import DSH
 from utils import read_fvecs
 
 np.random.seed(42)
@@ -11,14 +12,19 @@ def compare_KDTree_and_LSH(sample_num=1):
     file_path = "./datasets/{}/{}_base.fvecs".format("sift", "sift")
     vectors = read_fvecs(file_path)
     dim = vectors.shape[1]
+    print("begin building LSH")
     lsh = LSH(vectors, 64)
+    print("finish building LSH")
+    print("begin building DSH")
+    dsh = DSH(vectors, 64)
+    print("finish building DSH")
     start_time = time.time()
     tree = KDTree(vectors)
     print("KDTree building time: {} seconds".format(time.time() - start_time))
     probe = np.random.rand(dim,) * (np.linalg.norm(vectors[0]) + np.linalg.norm(vectors[10] + np.linalg.norm(vectors[20])))\
         / 3 / np.sqrt(dim) * 2.4
     
-    LSH_time, KDTree_time, c = 0., 0., 0.
+    LSH_time, DSH_time, KDTree_time, c_LSH, c_DSH = 0., 0., 0., 0., 0.
 
     for _ in range(sample_num):
         probe = np.random.rand(dim,) * (np.linalg.norm(vectors[0]) + np.linalg.norm(vectors[10] + np.linalg.norm(vectors[20])))\
@@ -33,6 +39,13 @@ def compare_KDTree_and_LSH(sample_num=1):
         LSH_time += time.time() - start_time
         # print("LSH time: {} seconds".format(time.time() - start_time))
 
+        # DSH
+        start_time = time.time()
+        hamming_distances = dsh.hamming_distances(probe)
+        nearest_point_idx = np.argsort(hamming_distances)[0]
+        distance_DSH = np.linalg.norm(probe - vectors[nearest_point_idx])
+        DSH_time += time.time() - start_time
+
         # KDTree
         start_time = time.time()
         distance, nearest_point = tree.nearest_neighbor(probe)
@@ -40,10 +53,13 @@ def compare_KDTree_and_LSH(sample_num=1):
         # print("KDTree time: {} seconds".format(time.time() - start_time))
 
         # print("c=", distance_LSH / distance)
-        c += distance_LSH / distance
+        c_LSH += distance_LSH / distance
+        c_DSH += distance_DSH / distance
 
     print("LSH mean time: {} seconds".format(LSH_time / sample_num))
+    print("DSH mean time: {} seconds".format(DSH_time / sample_num))
     print("KDTree mean time: {} seconds".format(KDTree_time / sample_num))
-    print("LSH mean approx rate time: {} seconds".format(c / sample_num))
+    print("LSH mean approx rate time: {} seconds".format(c_LSH / sample_num))
+    print("DSH mean approx rate time: {} seconds".format(c_DSH / sample_num))
 
 compare_KDTree_and_LSH(20)
